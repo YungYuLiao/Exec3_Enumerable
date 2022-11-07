d1257769c67ec7cb499c5fb2ce409b5c96aa9c2f
YungYuLiao <h11830123@gmail.com>
2022-11-07 19:57:59 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Q1 利用Enumerable.Range() 建立一個序列, 值介於 [1, 10]  請利用迴圈列出每一個項目值,確認無誤
+			var result = Enumerable.Range(1, 10);
+			foreach (var item in result)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//用Enumerable.Range() 建立一個序列, 值介於 [10, 15] 請利用迴圈列出每一個項目值,確認無誤
+
+			var result = Enumerable.Range(10,6);
+			foreach (var item in result)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
